02088584d2e4346802485da4ac349fcb4c69a459
suxql <20226410@s.ubaguio.edu>
2026-06-22 16:35:37 +0800

    extras screen + esc

--- scripts/ui/start_menu.gd
+++ scripts/ui/start_menu.gd
@@ -2,7 +2,13 @@ extends Control
 
 
 func _ready():
-    %new_game.pressed.connect(play)
+    %new_game.pressed.connect(newGame)
+    %extras.pressed.connect(extras)
 
-func play():
+func newGame():
     get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func extras():
+    get_tree().change_scene_to_file("res://scenes/ui/extras_screen.tscn")
+
+
