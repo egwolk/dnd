d79d7c9f7d4b0433c658933fcf366bc1fd6178f1
suxql <20226410@s.ubaguio.edu>
2026-06-22 17:36:45 +0800

    config screen

--- scripts/ui/start_menu.gd
+++ scripts/ui/start_menu.gd
@@ -1,14 +1,18 @@
 extends Control
 
 
-func _ready():
+func _ready() -> void:
     %new_game.pressed.connect(newGame)
     %extras.pressed.connect(extras)
+    %config.pressed.connect(config)
 
-func newGame():
+func newGame() -> void:
     get_tree().change_scene_to_file("res://scenes/main.tscn")
 
-func extras():
+func extras() -> void:
     get_tree().change_scene_to_file("res://scenes/ui/extras_screen.tscn")
+
+func config() -> void:
+    get_tree().change_scene_to_file("res://scenes/ui/config_screen.tscn")
 
 
