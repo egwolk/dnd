7f87aaa1a3e0fbeee9a3115210fe435576eebd8a
suxql <20226410@s.ubaguio.edu>
2026-06-29 21:21:54 +0800

    integrate temp media for tetsing and game pause functionality

--- modules/UI/pause_menu/pause_controller.gd
+++ modules/UI/pause_menu/pause_controller.gd
@@ -10,6 +10,12 @@ func _ready() -> void:
     add_transition( pause_state, config_state, &"CONFIG_SELECTED" )
     add_transition( ANYSTATE, pause_state, &"CANCELLED")
 
-func _update(_delta: float) -> void:
+func _enter() -> void:
+    get_tree().paused = true
+
+func _exit() -> void:
+    get_tree().paused =  false
+
+func _unhandled_input(_event: InputEvent) -> void:
     if Input.is_action_just_pressed("ui_cancel"):
         dispatch( &"CANCELLED" )
