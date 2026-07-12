46aac053e3417d81e3c38b8912a267b8b8e8c716
suxql <20226410@s.ubaguio.edu>
2026-07-12 23:08:59 +0800

    TEMP create base class for map nodes to reduce redunancy. will change it to a helper class later to get rid of coupling

--- modules/elements/map_node_boss/boss_view_state.gd
+++ modules/elements/map_node_boss/boss_view_state.gd
@@ -1,36 +1,12 @@
-class_name boss_view_state extends LimboState
+class_name boss_view_state extends map_base_node_view_state
 
-@onready var boss_ui = $boss_view
-@onready var map_state = %map_view_state
-
-func _setup() -> void:
-   boss_ui.visible = false
-
-func _enter() -> void:
-    boss_ui.visible = true
-
-func _exit() -> void:
-    boss_ui.visible = false
-
-func  _unhandled_input(_event: InputEvent) -> void:
-    if Input.is_action_just_pressed("ui_cancel"):
-        frozen_bg()
-        dispatch( &"PAUSE" )
-    if Input.is_action_just_pressed("m_pressed"):
-        frozen_bg()
-        map_state.can_dismiss = true
-        dispatch(&"MAP_SELECTED")
+func _get_ui_node() -> Control:
+	return $boss_view
 
 func _on_lose_test_pressed() -> void:
-    frozen_bg()
-    dispatch(&"LOSE_SELECTED")
-
+	frozen_bg()
+	dispatch(&"LOSE_SELECTED")
 
 func _on_win_test_pressed() -> void:
-    frozen_bg()
-    dispatch(&"WIN_SELECTED")
-
-func frozen_bg() -> void:
-    var img = get_viewport().get_texture().get_image()
-    var tex = ImageTexture.create_from_image(img)
-    Events.pause_background_captured.emit(tex)
+	frozen_bg()
+	dispatch(&"WIN_SELECTED")
--- modules/elements/map_node_event/event_view_state.gd
+++ modules/elements/map_node_event/event_view_state.gd
@@ -1,31 +1,8 @@
-class_name event_view_state extends LimboState
+class_name event_view_state extends  map_base_node_view_state
 
-@onready var event_ui = $event_view
-@onready var map_state = %map_view_state
-
-func _setup() -> void:
-    event_ui.visible = false
-
-func _enter() -> void:
-   event_ui.visible = true
-
-func _exit() -> void:
-    event_ui.visible = false
-
-func  _unhandled_input(_event: InputEvent) -> void:
-    if Input.is_action_just_pressed("ui_cancel"):
-        frozen_bg()
-        dispatch( &"PAUSE" )
-    if Input.is_action_just_pressed("m_pressed"):
-        frozen_bg()
-        map_state.can_dismiss = true
-        dispatch(&"MAP_SELECTED")
+func _get_ui_node() -> Control:
+	return $event_view
 
 func _on_next_node_pressed() -> void:
-    map_state.can_dismiss = false
-    dispatch(&"MAP_SELECTED")
-
-func frozen_bg() -> void:
-    var img = get_viewport().get_texture().get_image()
-    var tex = ImageTexture.create_from_image(img)
-    Events.pause_background_captured.emit(tex)
+	map_state.can_dismiss = false
+	dispatch(&"MAP_SELECTED")
